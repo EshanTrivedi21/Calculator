320e3e5757708c7b88d9f2c1cb8f999e7a0f73fc
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2022-05-05 14:07:02 +0530

    Front-ENd Design Changes

---
+++ script.js
@@ -0,0 +1,39 @@
+class Calculator{
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+    clear(){
+        this
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+
+    }
+
+    chooseOperation(operation){
+
+    }
+    compute(){
+
+    }
+
+    updateDisplay(){
+
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operations]')
+const equalsButton = document.querySelector('[data-equals]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const deleteButton = document.querySelector('[data-delete]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperand )
